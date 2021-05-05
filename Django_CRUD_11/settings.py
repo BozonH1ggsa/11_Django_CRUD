4f6be958c67f9eafd2ecb3053ac2ed50929ab82f
BozonH1ggsa <popovs.dmitrijs@gmail.com>
2021-05-05 23:16:12 +0300

    newlines and DEBUG=False

--- Django_CRUD_11/settings.py
+++ Django_CRUD_11/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-secure-kfjihek903sr^@pbe!z66a8omg%4644s2s$pgunoo$--oy6gv1klrj=tw)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
